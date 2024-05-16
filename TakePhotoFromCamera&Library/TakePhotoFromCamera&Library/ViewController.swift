445f39295d2d7bd962e9746b9765e7e8170f8a08
TLiLi HAMDI <tlilios@yahoo.com>
2024-05-16 18:36:16 +0200

    End

--- TakePhotoFromCamera&Library/TakePhotoFromCamera&Library/ViewController.swift
+++ TakePhotoFromCamera&Library/TakePhotoFromCamera&Library/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet weak var imgPhoto: UIImageView!
     override func viewDidLoad() {
@@ -21,14 +21,38 @@ class ViewController: UIViewController {
     
     func showPhotoAlert() {
         let alert = UIAlertController(title: "Take Photo From", message: nil, preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { action in
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { action in self.getPhoto(type: .camera)
             
         }))
-        alert.addAction(UIAlertAction(title: "Photo Librery", style: .default, handler: { action in
+        alert.addAction(UIAlertAction(title: "Photo Librery", style: .default, handler: { action in self.getPhoto(type: .photoLibrary)
             
         }))
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         present(alert, animated: true, completion: nil)
+    }
+    func getPhoto(type: UIImagePickerController.SourceType) {
+        let picker = UIImagePickerController()
+        picker.sourceType = type
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        dismiss(animated: true, completion: nil)
+//        if let image = info[.originalImage] as? UIImage {
+//            imgPhoto.image = image
+//        } else {
+//            print(" Image not found!!!")
+//        }
+        // We coul use image = info[.originalImage]
+        guard let image = info[.editedImage] as? UIImage else {
+            print("Image not found!!!")
+            return
+        }
+        imgPhoto.image = image
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 }
 
