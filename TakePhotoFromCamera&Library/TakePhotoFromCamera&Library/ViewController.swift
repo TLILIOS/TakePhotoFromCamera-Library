0cea7791e22c8681ee8023d3e1bc1ebd920834c7
TLiLi HAMDI <tlilios@yahoo.com>
2024-05-16 11:45:29 +0200

    Start

---
+++ TakePhotoFromCamera&Library/TakePhotoFromCamera&Library/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TakePhotoFromCamera&Library
+//
+//  Created by HTLILI on 16/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
